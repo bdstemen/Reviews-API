b4870139b7e8d06a630fe1a823179e8c7659b40c
Ben Stemen <ben.d.stemen@gmail.com>
2022-12-29 18:02:40 -0500

    add model, controller for fetching review metadata

--- src/server/controllers/meta.ts
+++ src/server/controllers/meta.ts
@@ -9,9 +9,44 @@ const meta = (req, res) => {
     product_id: req.query.product_id
   };
 
-  models.meta(reqData)
+  return Promise.all([
+    models.meta.ratings(reqData),
+    models.meta.recommended(reqData),
+    models.meta.characteristics(reqData)
+  ])
     .then((data) => {
-      res.status(200).send(data);
+      let ratingsData = data[0].rows;
+      let recommendedData = data[1].rows;
+      let characteristicsData = data[2].rows;
+
+      let ratings = {};
+      let recommended = {};
+      let characteristics = {};
+
+      ratingsData.forEach(rating => {
+        ratings[rating.rating] = parseInt(rating.count);
+      })
+
+      recommendedData.forEach(recommendation => {
+        recommended[recommendation.recommend.toString()] = parseInt(recommendation.count);
+      })
+
+      characteristicsData.forEach((characteristic) => {
+        characteristics[characteristic.name] = {
+          id: characteristic.id,
+          value: ((Math.round(parseFloat(characteristic.avg) * 1e4)) / 1e4)
+        };
+      })
+
+      let response = {
+        product_id: reqData.product_id,
+        ratings: ratings,
+        recommended: recommended,
+        characteristics: characteristics
+      };
+
+      console.log(response);
+      res.status(200).send(response);
     })
     .catch((err) => {
       res.status(404).send(err);
--- src/server/models/meta.ts
+++ src/server/models/meta.ts
@@ -1,8 +1,53 @@
 import pool from '../../database/index.js'
 
-const meta = (reqData) => {
-  return pool.query(`SELECT * FROM reviews WHERE product_id=${reqData.product_id} LIMIT 10;`)
+const meta = {
+  ratings: (reqData) => {
+    const query = `
+      SELECT COUNT(*), rating
+      FROM reviews
+      WHERE product_id = $1
+      GROUP BY rating
+      ORDER BY rating DESC`;
 
+    const data = [reqData.product_id];
+
+    return pool.query(query, data);
+  },
+
+  recommended: (reqData) => {
+    const query = `
+      SELECT
+        COUNT(*),
+        recommend
+      FROM reviews
+      WHERE product_id = $1
+      GROUP BY recommend`;
+
+    const data = [reqData.product_id];
+
+    return pool.query(query, data);
+  },
+
+  characteristics: (reqData) => {
+    const query = `
+      SELECT
+        AVG(value),
+        name,
+        characteristics.id
+      FROM characteristics_reviews
+      INNER JOIN characteristics
+      ON characteristics_reviews.characteristic_id = characteristics.id
+      WHERE characteristics.id
+      IN (
+        SELECT id
+        FROM characteristics
+        WHERE product_id = $1)
+      GROUP BY characteristics.id`;
+
+    const data = [reqData.product_id];
+
+    return pool.query(query, data);
+  }
 };
 
 export default meta;
